9ebda9096e62035d9628a0bba749a71c71db8702
iosdev20150920 <iosdev20150920@126.com>
2015-09-21 15:10:44 +0800

    webview demo 页面下载

---
+++ WebViewTest_20150921/WebViewDemo20150921/UCWebViewTest/AppDelegate.h
@@ -0,0 +1,24 @@
+/*
+ ***************************************************************************
+ * Copyright (C) 2005-2011 UC Mobile Limited. All Rights Reserved
+ * File              : AppDelegate.h
+ *
+ * Description   : 
+ *
+ * Creation       : 2015/06/25
+ * Author          : luyc@ucweb.com
+ * History         :
+ *                 Creation, 2015/06/25, luyc, Create the file
+ ***************************************************************************
+ **/
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewTest_20150921/WebViewDemo20150921/UCWebViewTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UCWebViewTest
+//
+//  Created by Sooyo on 15/6/25.
+//  Copyright (c) 2015年 Sooyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
